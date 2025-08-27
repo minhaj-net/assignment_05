398727528fc2bb7b2764cfc81d0e270481fea8ce
minhaj-net <infoglb.net@gmail.com>
2025-08-27 16:39:28 +0600

    Added frist cart interactive and proffesonal also responsive too

--- script/script.js
+++ script/script.js
@@ -14,6 +14,7 @@ function coinCountHandler(id) {
   const newCoinCount = coinCount - number;
   document.getElementById(id).innerText = newCoinCount;
 }
+//copy to mclipboard just copy handler
 
 document
   .getElementById("heart-icon-main")
@@ -22,20 +23,79 @@ document
     heartIconCount("heart-count");
   });
 
+// call button event handler
+
 document.getElementById("call-btn").addEventListener("click", function (e) {
   e.preventDefault;
   const nationalEmTitle =
     document.getElementById("national-em-title").innerText;
   const nationalEmNumber =
     document.getElementById("national-em-number").innerText;
+  const coinElement = document.getElementById("coin-count");
+  const number = 20;
+  const coinCount = parseInt(coinElement.innerText);
+  const newCoinCount = coinCount - number;
+  if (newCoinCount < 0) {
+    newCoinCount = 0;
+  }
+  coinElement.innerText = newCoinCount;
 
-  coinCountHandler("coin-count");
-  const coinCount = parseInt(document.getElementById("coin-count").innerText);
-  if (coinCount >= 19) {
-    alert("Calling  " + nationalEmTitle + "  " + nationalEmNumber);
-  } else if (coinCount <= 18) {
+  if (newCoinCount >= number) {
+    alert("📞 Calling " + nationalEmTitle + " " + nationalEmNumber);
+  } else if (newCoinCount < number) {
     alert(
-      "You don't have enough coins.You need at least 20 coins to make a call"
+      "⚠️ You don't have enough coins. You need at least 20 coins to make a call."
     );
   }
+
+  const historySlide = document.getElementById("history-container");
+
+  const newCart = document.createElement("div");
+  newCart.classList.add("new-cart-container");
+  newCart.innerHTML = `
+    <div class="flex justify-between items-center mb-4 rounded-md p-6 mt-6  bg-[#f5fff6]">
+      <div>
+        <h3 class="text-[#111111] font-semibold text-[18px]">${nationalEmTitle}</h3>
+        <p class="text-[18px] text-[#4A4A4A]">${nationalEmNumber}</p>
+      </div>
+      <div>
+        <h4 class="text-[#111111] text-[18px]">
+          <span>11.40.25</span>PM
+        </h4>
+      </div>
+    </div>
+  `;
+
+  historySlide.append(newCart);
+});
+
+// aside section clear button handler
+document
+  .getElementById("clear-btn-handler")
+  .addEventListener("click", function (e) {
+    e.preventDefault;
+    const historySlide = document.getElementById("history-container");
+    historySlide.remove();
+  });
+
+// copy button handler
+document.getElementById("copy-btn").addEventListener("click", function (e) {
+  e.preventDefault;
+  const copyCount = parseInt(document.getElementById("copy-count").innerText);
+  const number = 1;
+  const nationalEmNUmberCopy = parseInt(
+    document.getElementById("national-em-number").innerText
+  );
+
+  alert("Number has been copied  " + nationalEmNUmberCopy);
+  const copyNumberInrease = copyCount + number;
+
+  document.getElementById("copy-count").innerText = copyNumberInrease;
+
+  navigator.clipboard.writeText(nationalEmNUmberCopy);
+  then(() => {
+    alert("Text copied to clipboard!"); // successful হলে alert
+  }).catch((err) => {
+    console.error("Failed to copy text: ", err);
+  });
 });
